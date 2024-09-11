9894df5d84c2c4cbfb50b38547220533175a06d6
HaborK1 <mrr.egor.sychev@mail.ru>
2024-09-11 16:11:42 +1000

    Updates

--- assets/page-block-a9eqh9aH.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-RK_GQrt2.js";const i="_main_1kcdk_1",m={main:i};function t(n){const{children:s}=n;return a.jsx("main",{className:m.main,children:s})}export{t as P};
---
+++ assets/page-block-rLlrmB5Y.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-FQUiHId6.js";const i="_main_1kcdk_1",m={main:i};function t(n){const{children:s}=n;return a.jsx("main",{className:m.main,children:s})}export{t as P};

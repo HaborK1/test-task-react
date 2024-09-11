3a811bdd5608c9d2cffc74862e56eb206b959fd6
HaborK1 <mrr.egor.sychev@mail.ru>
2024-09-11 15:54:46 +1000

    Updates

---
+++ assets/wrong-page-o2pQTxUp.js
@@ -0,0 +1,1 @@
+import{j as r,L as s,r as o}from"./index-RK_GQrt2.js";import{P as e}from"./page-block-a9eqh9aH.js";function a(){return r.jsxs(e,{children:[r.jsx(s,{to:o.main(),children:"Перейти на главную страницу"}),r.jsx("h1",{children:"такой страницы не существует"})]})}export{a as default};
